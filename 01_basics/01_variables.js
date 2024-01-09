48f073eed9183dcb9a39e9959cea9a97f5bf9b34
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-09 06:37:03 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14455
+let accountEmail = "neerajraje@gmail.com"
+var accountpassword = "860013"
+accountCity = "Pune"
+let accountState;
+
+accountEmail = "hc@gmail.com";
+accountpassword= "55555";
+accountCity = "banglore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS Code as newer Version
+
+// alert("Hello"); // we are using nodejs,not browser
+
+console.log(3+3); 
+
+console.log("Neeraj");
+
+let name = "Neeraj";
+let age = 18
+let islog = false 
+let state;
+
+
+// Number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+//null => Standalone value 
+//undefined =>
+//symbol => unique 
+
+// Object
+
+console.log(typeof undefined); // o/p => undefined 
+
+console.log(typeof undefined); // o/p => object
