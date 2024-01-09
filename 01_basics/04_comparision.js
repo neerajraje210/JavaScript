0bc052e177ceaea6443d444785e6688c6acaf963
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-09 11:24:41 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log("2">1);
+
+console.log("02">1 );
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// *notes
+//Equality check == and comparisions > < >= <= works Differently
+// Comparision converts null to a number ,treating it as 0.
+// Thats why *** null >= 0 *** is true and *** null > 0 *** is false.
+
+
+console.log(undefined == 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// strict check ( === )
+
+console.log("2"===2 );
+   
