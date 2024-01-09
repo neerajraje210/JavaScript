05e8e78e8a403e4dfd5505f9009d1c2f012b0d15
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-09 08:29:20 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Neeraj"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "354" => 354 converts to number 
+//"352ahs" => NaN (not a number )
+// true => converts to 1;
+//false => converts to 0;
+
+let isloggedIn = "Neeraj"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//if we write 1 it is converted to True=>  1 =>true 
+// if we write 0 then it is converted to False =>  0=> false
+//if empty " " then it is converted to => false  
+//if we write "name" then it is converted to => True
+
+
+let someNumber = 35;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
