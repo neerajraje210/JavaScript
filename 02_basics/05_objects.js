3feab73e90cdddb168de2dfdf18d571eff8aa364
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-16 13:34:04 +0000

    json breif

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,25 @@
+const course = {
+    name: "JS",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: inst} =course
+
+console.log(inst);
+
+// {
+//     "name":"neeraj",
+//     "coursename":"jshindn",    *****JSON*****
+//     "price":"free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
+ 
