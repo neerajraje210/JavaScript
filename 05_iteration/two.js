57a9bc5a4b4ee7df0879d5a711deca5532d0fe49
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-24 06:29:48 +0000

    whileloop

---
+++ 05_iteration/two.js
@@ -0,0 +1,26 @@
+let index = 0;
+
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index = index + 3
+}
+
+console.log("______________________________________________________________________");
+
+let myarray = ['flash','batman','spiderman']
+
+let arr = 0
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr +1
+}
+
+console.log("------------------------------------------------------------------------------");
+
+let score =11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
