49b8bfb9bc06948895f5d2923a8e51ee69f2bca2
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-01-25 05:54:29 +0000

    For Each loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,47 @@
+const coding = ["js","ruby","java","python","cpp"] 
+
+ coding.forEach( function (val) {
+
+    console.log(val);
+ })
+
+ coding.forEach( (item) => {
+     console.log(item);
+ } )
+
+
+ function printme(item){
+    console.log(item);
+ }
+
+ coding.forEach(printme)
+
+
+ coding.forEach((item ,index ,arr)=>{
+
+    console.log(item,index,arr);
+
+ })
+
+ const myCoding = [
+    {
+        languageName: "javaScript",
+        languageFilename: "js"
+    },
+
+    {
+        languageName: "Java",
+        languageFilename: " JAVA "
+    },
+
+    {
+        languageName:"Python",
+        languageFilename:"Py"
+    }
+ ]
+
+
+ myCoding.forEach((item)=>{
+      
+       console.log(item.languageName);
+ })
