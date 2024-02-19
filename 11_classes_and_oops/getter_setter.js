f486b5a97fb41fa7edbd4cc751597bb2546bcbc2
neerajraje210 <78420795+neerajraje210@users.noreply.github.com>
2024-02-19 07:39:24 +0000

    Getter_Setter

---
+++ 11_classes_and_oops/getter_setter.js
@@ -0,0 +1,24 @@
+class User{
+    constructor(email,password){
+        this.email=email
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+         this._email = value
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+
+    set password(value){
+        this._password = value.toUpperCase()
+    }
+}
+
+const neeraj = new User("Neeraj@gmail.com","abc")
+console.log(neeraj.password);
